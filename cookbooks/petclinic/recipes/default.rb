6060241c97ecfd4ab34cc6f2888b0e29a449049d
RobertKelly <robert@liatrio.com>
2017-01-10 16:13:54 -0800

    adding local IP address

--- cookbooks/petclinic/recipes/default.rb
+++ cookbooks/petclinic/recipes/default.rb
@@ -9,7 +9,7 @@ env_vars [{'JAVA_OPTS' =>
   '-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true '}]
 end
 
-remote_file '/opt/tomcat_petclinic/webapps/petclinic.war' do
+remote_file '/opt/tomcat_petclinicserver/webapps/petclinic.war' do
 #source 'http://your.artifactory.server:8081/path/to/petclinic/pettclinic-1.0.0-SNAPSHOT.war'
 source 'http://192.168.50.4:8081/artifactory/libs-snapshot-local/org/springframework/samples/spring-petclinic/1.0.0-SNAPSHOT/spring-petclinic-1.0.0-20170110.180153-1.war'
 owner 'tomcat_petclinicserver'
